7884bbe061280d72247bc9a4fa6c9998a1d99a31
mateuspaduan <mateusplc@hotmail.com>
2024-05-19 16:21:11 -0300

    Implementation of account controller tested

--- routes/account.js
+++ routes/account.js
@@ -2,47 +2,47 @@ const express = require("express");
 const accountController = require("../controllers/accountController.js");
 const accountsRouter = express.Router();
 
-async function createAcc (req, res) {
+async function create (req, res) {
     const accountName = req.body.accountName;
     if (!(accountName)) {
-        res.status(400).send('All the inputs are required!');
+        res.sendStatus(400).send('All the inputs are required!');
     }
     const newAccount = await accountController.create(accountName);
     if (!(newAccount)) {
-        res.status(400).send('An account with that name already exists.');
+        res.sendStatus(400).send('An account with that name already exists.');
     }
-    res.status(200).send(`Account ${accountName} created successfully!`);
+    res.sendStatus(200).send(`Account ${accountName} created successfully!`);
 };
 
 async function findByName(req, res) {
     const accountName = req.body.accountName;
     if (!(accountName)) {
-        res.status(400).send('All the inputs are required!');
+        res.sendStatus(400).send('All the inputs are required!');
     }
     const account = await accountController.findByName(accountName);
-    res.send(200).json({message: account});
+    res.sendStatus(200).json({message: account});
 };
 
 async function editByName (req, res) {
     const currentAccountName = req.body.currentAccountName;
     const newAccountName = req.body.newAccountName;
     if (!(currentAccountName)) {
-        res.status(400).send('All the inputs are required!');
+        res.sendStatus(400).send('All the inputs are required!');
     }
     const accountToEdit = await accountController.editByName(currentAccountName, newAccountName);
-    res.send(200).json({message: accountToEdit});
+    res.sendStatus(200).json({message: 'Account edited successfully!'});
 };
 
 async function deleteByName (req, res) {
     const accountName = req.body.accountName;
     if (!(accountName)) {
-        res.status(400).send('All the inputs are required!');
+        res.sendStatus(400).json({message: 'All the inputs are required!'});
     }
     const accountToDelete = await accountController.deleteByName(accountName);
-    res.send(200).json({message: accountToDelete});
+    res.sendStatus(200).json({message: 'Account deleted successfully!'});
 };
 
-accountsRouter.post("/", createAcc);
+accountsRouter.post("/", create);
 accountsRouter.get("/", findByName);
 accountsRouter.put("/", editByName);
 accountsRouter.delete("/", deleteByName);
